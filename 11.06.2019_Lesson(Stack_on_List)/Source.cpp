e536cccf3159e22432c9322b72f42654fd84166d
айкеновад <@205_8>
2019-06-11 09:18:26 +0600

    Добавьте файлы проекта.

---
+++ 11.06.2019_Lesson(Stack_on_List)/Source.cpp
@@ -0,0 +1,14 @@
+﻿#include<list>
+#include<vector>
+#include<stack>
+using namespace std;
+
+int main() {
+
+	stack<list<int>, int> s; //на без листа, т.е. будут вызываться методы для листа и память будет распределяться как в листе
+	stack<vector<int>, int> s1;// на базе вектора
+	stack<deque<int>, int> s2;//на базе двусторонней очереди
+
+	system("pause");
+	return 0;
+}
